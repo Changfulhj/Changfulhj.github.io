7034fa5cbde86a02272c416f1124115b33f5ab08
halo <your_email@example.com>
2023-05-16 19:36:32 +0800

    Site updated: 2023-05-16 19:36:32

--- js/halo.js
+++ js/halo.js
@@ -8,8 +8,3 @@ site_title.style.fontSize = '25px'
 // 底部颜色
 var footerDom = document.querySelector('#footer-wrap')
 footerDom.style.backgroundColor = "#002e46"
-
-// 取消首页遮罩
-window.onload = ()=> {
-    document.querySelector("#page-header.full_page").style.backgroundColor = "transparent";
-}
