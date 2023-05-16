65d86df485892f52c5fa9d4d7372bbe0ed4f98c5
halo <your_email@example.com>
2023-05-16 18:06:11 +0800

    Site updated: 2023-05-16 18:06:10

---
+++ js/halo.js
@@ -0,0 +1,8 @@
+
+//更改
+
+document.querySelector('.site-name').textContent='白河寒秋'
+var site_title = document.querySelector('#site-title')
+site_title.textContent = '你将蓝色的地中海唤醒，而它曾昏睡了一整个夏天，被澄澈水流的回旋催眠入梦'
+site_title.style.fontSize = '20px'
+
