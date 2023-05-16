f220b9cc23d21ea6b12281ef69cddc8445b81df8
halo <your_email@example.com>
2023-05-16 18:08:44 +0800

    Site updated: 2023-05-16 18:08:43

--- js/halo.js
+++ js/halo.js
@@ -3,6 +3,6 @@
 
 document.querySelector('.site-name').textContent='白河寒秋'
 var site_title = document.querySelector('#site-title')
-site_title.textContent = '你将蓝色的地中海唤醒，而它曾昏睡了一整个夏天，被澄澈水流的回旋催眠入梦'
-site_title.style.fontSize = '20px'
+site_title.textContent = '蓝夜将尽，天光无多，夏日已去   ----琼·狄迪恩'
+site_title.style.fontSize = '25px'
 
