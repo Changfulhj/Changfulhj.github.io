b714796edb516e5779ad72ca517f695105718fd4
halo <your_email@example.com>
2023-05-16 19:35:29 +0800

    Site updated: 2023-05-16 19:35:28

--- js/halo.js
+++ js/halo.js
@@ -1,7 +1,15 @@
 //更改
-
 document.querySelector('.site-name').textContent='白河寒秋'
 var site_title = document.querySelector('#site-title')
 site_title.textContent = '蓝夜将尽，天光无多，夏日已去   ----琼·狄迪恩'
 site_title.style.fontSize = '25px'
 
+
+// 底部颜色
+var footerDom = document.querySelector('#footer-wrap')
+footerDom.style.backgroundColor = "#002e46"
+
+// 取消首页遮罩
+window.onload = ()=> {
+    document.querySelector("#page-header.full_page").style.backgroundColor = "transparent";
+}
