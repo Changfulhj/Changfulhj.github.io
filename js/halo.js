9822eca80dfdf142ad5072a42bb32a88c36cfbdb
halo <your_email@example.com>
2023-05-17 15:18:10 +0800

    Site updated: 2023-05-17 15:18:09

--- js/halo.js
+++ js/halo.js
@@ -2,8 +2,3 @@
 // 底部颜色
 var footerDom = document.querySelector('#footer-wrap')
 footerDom.style.backgroundColor = "#002e46"
-
-// 取消首页遮罩
-window.onload = ()=> {
-    document.querySelector("#page-header.full_page").style.backgroundColor = "transparent";
-}
